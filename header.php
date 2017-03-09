564b0538ccc32eb27dd56bfe26369ba2fa4c31aa
unknown <zalemmm@yahoo.com"\n>
2017-03-09 17:07:40 +0100

    accordeon

--- header.php
+++ header.php
@@ -20,11 +20,24 @@
 
 	<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 	<style type="text/css" media="screen">@import url( <?php echo bloginfo("template_url") ?>/css/style.css?d=11062012 );</style>
-	
+
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_head(); ?>
 
+	<!--load jquery & jquery-ui-->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
+
+	<!--jquery ui accordion-->
+	<script>
+	$( function() {
+		$( "#accordion" ).accordion({
+			active: false,
+	  	collapsible: true,
+		});
+	} );
+	</script>
 
 	<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/pokaz.js"></script>
 	<!--[if lte IE 7]>
