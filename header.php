d04e60f148208d4d2cdcd193b76f2f9aed82b20c
unknown <zalemmm@yahoo.com"\n>
2017-03-13 17:36:27 +0100

    Maj des styles / conformité au prod

--- header.php
+++ header.php
@@ -28,6 +28,7 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 	<!--jquery & jquery-ui-->
+
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
