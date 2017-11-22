b26018a9d2b89dd1b3a9a2c91f30a526e5911966
unknown <zalemmm@yahoo.com"\n>
2017-11-22 09:21:02 +0100

    carn et d'adresse

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
-
-
-
-
 //------------------------------------------------------------------------------
 //                                                                        JQUERY
 //------------------------------------------------------------------------------
 jQuery(document).ready(function ($) {
 	//////////////////////////////////////////////////////////////// quantité+- //
   $("#spinner").spinner();
-
-
-
-
 
   //////////////////////////////////////////// bouton close messages d'erreur //
   //////////////////////////////////////////////////////////////////////////////
@@ -117,10 +109,6 @@ jQuery(document).ready(function ($) {
     });
   });
 
-
-
-
-
   ///////////////////////////////////////////////////////////// smooth scroll //
   //////////////////////////////////////////////////////////////////////////////
 
@@ -203,7 +191,44 @@ jQuery(document).ready(function ($) {
     });
   }
 
-  ///////////////////////////////////// affichage conditionnel mobile/desktop //
+  /////////////////////////////////////////////////////////// toggle adresses //
+  $(".checkbox").change(function() {
+    if(this.checked) {
+      $(".blocAdresse").removeClass('blocSelect');
+      $('.blocAdresseSelect').removeClass('headSelect');
+      $('.adresseLiv').hide();
+
+      $(this).closest(".blocAdresse").addClass('blocSelect');
+      $(this).closest('.blocAdresseSelect').addClass('headSelect');
+    }
+    $('#adresseCheck').submit();
+  });
+
+  var check = $('#adresseCheck');
+  check.submit(function (e) {
+    e.preventDefault();
+
+    $.ajax({
+      type: check.attr('method'),
+      url: check.attr('action'),
+      data: check.serialize(),
+      success: function (data) {
+
+      },
+      complete: function(data) {
+        var r = confirm('L\'adresse de livraison a bien été mise à jour. Retourner sur votre commande ?');
+        if (r == true) {
+          window.location.href = check.attr('action');
+        } else {
+          window.location.href = window.location.href
+        }
+      },
+      error: function (data) {
+        alert('une erreur s\'est produite, veuillez réessayer.');
+      },
+    });
+  });
+  //----------------------------------- affichage conditionnel mobile/desktop //
   //////////////////////////////////////////////////////////////////////////////
   var isDesktop = window.matchMedia("only screen and (min-width: 1024px)");
 
@@ -232,8 +257,8 @@ jQuery(document).ready(function ($) {
       }
     });
 
-		/////////////////////////////////////////////////////////////// sticky menu //
-		//////////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////// sticky menu //
+		////////////////////////////////////////////////////////////////////////////
 
 		$(window).scroll(function() {
 	    if ($(window).scrollTop() > 150) {
@@ -250,30 +275,45 @@ jQuery(document).ready(function ($) {
 		});
 
     ////////////////////////////////////////////////////////////////// Promos //
-    ////////////////////////////////////////////////////////////////////////////
+    //---------------------------------------------------------------- gauche //
     var imageLeft = new Array ();
     imageLeft[0] = '<img class="iz1"  src="https://www.france-banderole.com/wp-content/themes/fb/images/promoAkilux.png" title="panneaux akilux pas cher" alt="akilux agence immobilière" />';
     imageLeft[1] = '<img class="iz1" src="https://www.france-banderole.com/wp-content/themes/fb/images/promoGoutte.png" title="promo oriflamme beachflag" alt="promo oriflamme" />';
     imageLeft[2] = '<img class="iz1"  src="https://www.france-banderole.com/wp-content/themes/fb/images/promoRollup.png" title="Rollup meilleur prix: 28€" alt="promo kakemono rollup" />';
-
+    imageLeft[3] = '<img class="iz1" src="https://www.france-banderole.com/wp-content/themes/fb/images/promoKakemono2.png" title="kakemono tissu, totem plv" alt="kakemono tissu pas cher" />';
+	/*imageLeft[4] = '<img class="iz1"  src="https://www.france-banderole.com/wp-content/themes/fb/images/promoLeftstand.png" title="Stand Expo Bag Promo" alt="Stand Promotion" />';
+	imageLeft[5] = '<img class="iz1"  src="https://www.france-banderole.com/wp-content/themes/fb/images/affichefluoLeft.png" title="Affiche fluo Promo" alt="Affiche 120gr Fluo" />';
+	imageLeft[6] = '<img class="iz1"  src="https://www.france-banderole.com/wp-content/themes/fb/images/affichequadriLeft.png" title="Affiche quadri" alt="Affiche 120gr dos bleu" />';*/
 
     var linkLeft = new Array ();
     linkLeft[0] = '<a href="https://www.france-banderole.com/panneaux-akilux" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
     linkLeft[1] = '<a href="https://www.france-banderole.com/oriflammes" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
     linkLeft[2] = '<a href="https://www.france-banderole.com/roll-up" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
+    linkLeft[3] = '<a href="https://www.france-banderole.com/totem" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
+	/*linkLeft[4] = '<a href="https://www.france-banderole.com/plv-interieur" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
+	linkLeft[5] = '<a href="https://www.france-banderole.com/promotions" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
+	linkLeft[6] = '<a href="https://www.france-banderole.com/promotions" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';*/
 
-
+    //---------------------------------------------------------------- droite //
     var imageRight = new Array ();
     imageRight[0] = '<img class="iz1"  src="https://www.france-banderole.com/wp-content/themes/fb/images/promoStands.png" title="meilleur prix stand parapluie tissu tendu : 257€ !" alt="stand tissu pas cher" />';
     imageRight[1] = '<img class="iz1" src="https://www.france-banderole.com/wp-content/themes/fb/images/promoLux.png" title="roll-up conçu pour durer" alt="roll-up luxe modulable" />';
     imageRight[2] = '<img class="iz1" src="https://www.france-banderole.com/wp-content/themes/fb/images/promoTente.png" title="meilleur prix tentes publicitaires : 347€ !" alt="tente publicitaire pas cher" />';
-
+    imageRight[3] = '<img class="iz1" src="https://www.france-banderole.com/wp-content/themes/fb/images/promoKakemono.png" title="kakemono tissu, totem plv" alt="kakemono tissu pas cher" />';
+	/*imageRight[4] = '<img class="iz1"  src="https://www.france-banderole.com/wp-content/themes/fb/images/promoRightstand.png" title="Stand Expo Bag Promo" alt="Stand Promotion" />';
+	imageRight[5] = '<img class="iz1"  src="https://www.france-banderole.com/wp-content/themes/fb/images/affichefluoRight.png" title="Affiche fluo Promo" alt="Affiche 120gr Fluo" />';
+	imageRight[6] = '<img class="iz1"  src="https://www.france-banderole.com/wp-content/themes/fb/images/affichequadriRight.png" title="Affiche quadri" alt="Affiche 120gr dos bleu" />';*/
 
     var linkRight = new Array ();
     linkRight[0] = '<a href="https://www.france-banderole.com/stand-parapluie/" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
     linkRight[1] = '<a href="https://www.france-banderole.com/roll-up" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
     linkRight[2] = '<a href="https://www.france-banderole.com/tente-publicitaire-barnum" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
-
+    linkRight[3] = '<a href="https://www.france-banderole.com/totem" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
+	/*linkRight[4] = '<a href="https://www.france-banderole.com/plv-interieur" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
+	linkRight[5] = '<a href="https://www.france-banderole.com/promotions" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
+	linkRight[6] = '<a href="https://www.france-banderole.com/promotions" class="metaButton promo">Voir <i class="fa fa-caret-right"></i></a>';
+*/
+    //------------------------------------------------------------- randomize //
     var sizeL = imageLeft.length;
     var sizeR = imageRight.length;
     var x = Math.floor(sizeL*Math.random());
@@ -281,8 +321,8 @@ jQuery(document).ready(function ($) {
     $('.izoneInL').append(imageLeft[x]+linkLeft[x]);
     $('.izoneInR').append(imageRight[y]+linkRight[y]);
 
-		///////////////////////////////////////////////////////////////// addtocart //
-		//////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////// addtocart //
+		////////////////////////////////////////////////////////////////////////////
 		var frm = jQuery('#cart_form');
 		frm.submit(function (e) {
 			e.preventDefault();
@@ -305,6 +345,30 @@ jQuery(document).ready(function ($) {
 			});
 		});
 
+    //-------------------------------------------------addtocart modèle page PLV
+    $("[data-cartform]").submit(function(e) {
+        var frm2 = $(this);
+        var itemImg = $('.prom_sub');
+        e.preventDefault();
+
+       jQuery.ajax({
+         type: frm2.attr('method'),
+         url: frm2.attr('action'),
+         data: frm2.serialize(),
+         success: function (data) {
+           flyToElement(jQuery(itemImg), jQuery('.menu-client--panier'));
+         },
+         complete: function(data) {
+           location.href = location.href;
+         },
+         error: function (data) {
+           alert('une erreur s\'est produite, veuillez réessayer.');
+         },
+       });
+    });
+
+
+  //-----------------------------------------------------------------fin desktop
   }else{
 		var frm = jQuery('#cart_form');
 		frm.submit(function (e) {
@@ -331,7 +395,7 @@ jQuery(document).ready(function ($) {
 			});
 		});
 	}
-
+  //------------------------------------------------------------------fin mobile
 
   //----------------------------------------------------------script fly to cart
   function flyToElement(flyer, flyingTo) {
